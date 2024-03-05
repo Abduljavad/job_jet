c7da6cc96b2a23a872f389d1541544b7dbf79d29
Abdul javad <javad32@gmail.com>
2024-03-05 15:21:28 +0530

    fix:errorHandler added

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -2,16 +2,19 @@
 
 namespace App\Http\Middleware;
 
+use App\Http\Traits\ResponseTraits;
 use Illuminate\Auth\Middleware\Authenticate as Middleware;
 use Illuminate\Http\Request;
 
 class Authenticate extends Middleware
 {
+    use ResponseTraits;
+
     /**
      * Get the path the user should be redirected to when they are not authenticated.
      */
     protected function redirectTo(Request $request): ?string
     {
-        return $request->expectsJson() ? null : route('login');
+        return $request->expectsJson() ? null : $this->errorResponse('Not Found', 404);
     }
 }
