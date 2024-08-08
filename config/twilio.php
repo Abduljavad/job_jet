6b6a8c4ac0b8935e60cabd150a242ac5cefaec89
Abdul javad <javad32@gmail.com>
2024-08-09 01:55:56 +0530

    feat: default values for twilio added

--- config/twilio.php
+++ config/twilio.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'account_sid' => env('TWILIO_ACCOUNT_SID', ''),
-    'auth_token' => env('TWILIO_AUTH_TOKEN', ''),
-    'service_sid' => env('TWILIO_SERVICE_SID', ''),
+    'account_sid' => env('TWILIO_ACCOUNT_SID', 'ACb8a37a9bef044c9a546ab5ae880ff6bf'),
+    'auth_token' => env('TWILIO_AUTH_TOKEN', '18c80de45fe2ec9b0c761d80699cbe80'),
+    'service_sid' => env('TWILIO_SERVICE_SID', 'VA73caf35b9933d1319bcffe2a07daec4d'),
 ];
