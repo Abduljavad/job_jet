c54d4d719204d73766e4b95fdabe3d3a61837f07
Abdul Javad <javad32@gmail.com>
2025-01-20 17:02:59 +0530

    fix: twilio new service id added

--- config/twilio.php
+++ config/twilio.php
@@ -2,6 +2,6 @@
 
 return [
     'account_sid' => env('TWILIO_ACCOUNT_SID', 'ACb8a37a9bef044c9a546ab5ae880ff6bf'),
-    'auth_token' => env('TWILIO_AUTH_TOKEN', 'baab4c7cbfd4f2731155de8026d7d30d'),
-    'service_sid' => env('TWILIO_SERVICE_SID', 'VA1aa611809791d703576a1ce0b60727dc'),
+    'auth_token' => env('TWILIO_AUTH_TOKEN', '321eae9c65aaa59095405b52423bef41'),
+    'service_sid' => env('TWILIO_SERVICE_SID', 'VA50b0d453d750b30726cd4ef27b0bbd39'),
 ];
